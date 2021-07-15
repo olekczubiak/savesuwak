ccedf6f4d11ea5e5ce2251bb46216f848c22e6ef
Aleksander Czubiak <olek@MacBook-Pro-Aleksander.local>
2021-07-15 14:35:57 +0200

    update sanding mail, and saving

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# savesuwak
+# Save Suwak – Newonce🤐
+Skrypt do zapisywania numerów pojawiających się w audycji "Funkowa środa Suwaka" w radiu Newonce. Dane są pobierane z API radia newonce. 
+> https://newonce.net/api/radio_history 
+
+Zapisane utwory są wysyłane na maila w celu lepszej organizacji. 
+
+![](https://i.imgur.com/e7oAUc7.jpg)
+
+### Env
+Wykorzystana skrzynka pocztowa jest to **gmail**
+W pliku env.py trzmane są dane do logowania do poczty:
+```
+SENDER_MAIL = 'example@gmail.com' # adres email z ktorego wysyłamy
+PASSWORD = 'password' # Hasło do skrzynki pocztowej z której otrzymujemy dane
+PORT = 587 - # port dla dla gmaila
+SMTP_SERVER = 'smtp.gmail.com' # dla gmaila
+RECIVER_MAIL = 'example@gmail.com' #adres na ktory wysyłamy składankę z dzisiaj
+```
+Jeżeli chcemy wysyłać na ten sam adres emailowy z którego wysyłamy, wystarczy dać ten sam adress co w **SENDER_MAIL**.
+
+### Instalacja
+Instalacje należy zacząć od zainstalowania biblioteki request. Następnie na serwerze za pomocą aplikacji **xxx** włączać skrypt main.py co środę w godzinach trwania audycji. 
+
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import smtplib, ssl
 from env import RECIVER_MAIL, SENDER_MAIL, PASSWORD, PORT, SMTP_SERVER
 
-MY_TIME = str(time.ctime()).replace(' ', '-')
+MY_TIME = str(time.ctime()).replace(' ', '-')[:10]
 fake_db = []
 
 
@@ -44,7 +44,7 @@ class Gmail(object):
 
 
 
-def save_to_file(data: str, filename: str = MY_TIME[:10]):
+def save_to_file(data: str, filename: str = MY_TIME):
     f = open(f'data/{filename}', 'a')
     f.write(f'{data} \n ')
 
@@ -56,14 +56,16 @@ def parse_data(json_data: str,number_of_song: int ,key_word: str):
 
 def return_all_data(json_data):
     for n in range(number_of_elements(json_data)):
-        title = str(parse_data(json_data, n, 'title').decode()).replace('"', '')
+        title = '<b>' + str(parse_data(json_data, n, 'title').decode()).replace('"', '') + '</b>'
+        title_to_save =  str(parse_data(json_data, n, 'title').decode()).replace('"', '')
         artist = str(parse_data(json_data, n, 'artist').decode()).replace('"', '')
         element = f'{artist} – {title}'
+        element_to_save = f'{artist} – {title_to_save}'
         if element in fake_db:
             pass
         else:
             fake_db.append(element)
-            save_to_file(element)
+            save_to_file(element_to_save)
 
 
 
@@ -72,21 +74,18 @@ if __name__ == "__main__":
     while True:
         n += 1
         return_all_data(download_data('https://newonce.net/api/radio_history'))
-        print(f'Próba numer = {n}')
-        print(fake_db)
 
         if n == 3:
             output: str = ''
             for element in fake_db:
                 output +=  '<li>'+ element + '</li>'
-            html = f"<html><head></head><body><p>Audycja z dzisiaj<br> <ol> {output.encode('ascii', 'ignore')[0:3] + output.encode('ascii', 'ignore')[6:]} </ol></body></html>"
-            # print(html)
+            html = f"<html><head></head><body><p>Audycja z dzisiaj<br> <ol> {output.encode('ascii', 'ignore')} </ol></body></html>"
             gm = Gmail(SENDER_MAIL, PASSWORD)
-            gm.send_message('TEST - 4', RECIVER_MAIL, html)
+            gm.send_message(f'Funkowa środa Suwaka {MY_TIME}' , RECIVER_MAIL, html)
             break
-        
+
         # Spi 15 piosenek średnio po 3 min czyli 15*180 = 2700 sec
-        # time.sleep(2700)
+        time.sleep(2700)
 
         # Testowo na 2 piosenki czyli 2 * 180 = 360 sec
-        time.sleep(360)
+        # time.sleep(360)
